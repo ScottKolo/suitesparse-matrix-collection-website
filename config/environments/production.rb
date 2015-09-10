5c99de23c49db0232ddfb0f2f8725c0c3cdaf53b
Scott Kolodziej <spkolodziej@gmail.com>
2015-09-10 18:51:50 -0500

    Fix quotation marks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Use environment variable for production secrets
-  config.secret_key_base = ENV[“SECRET_KEY_BASE”]
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
