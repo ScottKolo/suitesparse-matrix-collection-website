f92e6f76d8d0234b571b73244a2232300c16024b
readamus <readamus@cse.tamu.edu>
2015-10-15 15:58:24 -0500

    Removed unused pages helper. Added rspec test for pages controller.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
 
+  describe "showing the about page" do
+
+    it "should render the about page" do
+      get :about
+      expect(response).to render_template :about
+    end
+
+  end
+
 end
