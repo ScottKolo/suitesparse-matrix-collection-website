99b186a7cbf190f7a7e3ed68e0c65173793e8cee
Scott Kolodziej <spkolodziej@gmail.com>
2015-10-15 16:10:47 -0500

    More asset changes

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module UFSMCWeb
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
   end
 end
