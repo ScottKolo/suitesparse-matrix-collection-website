aeb205b83c2309977442f6dc65e1590f262e56ac
Scott Kolodziej <spkolodziej@gmail.com>
2015-10-20 16:12:40 -0500

    Fix download button

--- app/models/matrix.rb
+++ app/models/matrix.rb
@@ -1,2 +1,16 @@
 class Matrix < ActiveRecord::Base
+    def self.get_base_url
+        return '//www.cise.ufl.edu/research/sparse/'
+    end
+
+    def get_url(file_format)
+        base_url = Matrix.get_base_url
+        if file_format == "MAT"
+            return "#{base_url}mat/#{self.group}/#{self.name}.mat"
+        elsif file_format == "RB"
+            return "#{base_url}RB/#{self.group}/#{self.name}.tar.gz"
+        elsif file_format == "MM"
+            return "#{base_url}MM/#{self.group}/#{self.name}.tar.gz"
+        end
+    end
 end
