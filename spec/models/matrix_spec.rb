cca26ffa64a0e026919fc27c348a128f7f49a391
readamus <readamus@cse.tamu.edu>
2015-10-22 16:59:28 -0500

    Fixing matrix download test

--- spec/models/matrix_spec.rb
+++ spec/models/matrix_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe Matrix, type: :model do
     end
 
     it "should provide the correct MATLAB download URL" do
-        expect(@matrix.get_url(:matlab)).to eq("//www.cise.ufl.edu/research/sparse/mat/groupName_1/matrixName_1.mat")
+        expect(@matrix.get_url(:matlab)).to eq("http://www.cise.ufl.edu/research/sparse/mat/groupName_1/matrixName_1.mat")
     end
     it "should provide the correct Rutherford-Boeing download URL" do
-        expect(@matrix.get_url(:rutherford_boeing)).to eq("//www.cise.ufl.edu/research/sparse/RB/groupName_1/matrixName_1.tar.gz")
+        expect(@matrix.get_url(:rutherford_boeing)).to eq("http://www.cise.ufl.edu/research/sparse/RB/groupName_1/matrixName_1.tar.gz")
     end
     it "should provide the correct Matrix Market download URL" do
-        expect(@matrix.get_url(:matrix_market)).to eq("//www.cise.ufl.edu/research/sparse/MM/groupName_1/matrixName_1.tar.gz")
+        expect(@matrix.get_url(:matrix_market)).to eq("http://www.cise.ufl.edu/research/sparse/MM/groupName_1/matrixName_1.tar.gz")
     end
     it "should throw an exception if an unknown format is specified" do
 
