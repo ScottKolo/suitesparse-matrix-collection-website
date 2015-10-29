61f2146ba88e32ab1bf3fc15b3ef5e75cada16c9
readamus <readamus@cse.tamu.edu>
2015-10-29 13:59:03 -0500

    Connected the GUI filters to matrices controller. We now have primitive working filters.

--- app/controllers/matrices_controller.rb
+++ app/controllers/matrices_controller.rb
@@ -4,16 +4,38 @@ class MatricesController < ApplicationController
 
   def index
     parse_params
+    @matrices = Matrix.all
 
     # Apply Filter
+    filters = session[:filter]
+    if !filters.nil?
+      filters.each do |attribute, value|
+        if value.class == Hash then
+          if value["min"] != "" then
+            @matrices = @matrices.where(%{"#{attribute}" >= ?}, value["min"])
+          end
+          if value["max"] != "" then
+            @matrices = @matrices.where(%{"#{attribute}" <= ?}, value["max"])
+          end
+        elsif value != "" then
+          if value == "on" then
+            value = "true"
+          end
+          @matrices = @matrices.where(%{"#{attribute}" = ?}, value)
+        end
+      end
+    end
 
     # Apply Sort
     s = session[:sort].nil? ? nil : %{"#{session[:sort]}"};
+    @matrices = @matrices.order(s)
+
+    # Apply pagination
     @per_page = params[:per_page] || Matrix.per_page || 20
     if @per_page == "All"
       @per_page = Matrix.count
     end
-    @matrices = Matrix.order(s).paginate(:page => params[:page], :per_page => @per_page)
+    @matrices = @matrices.paginate(:page => params[:page], :per_page => @per_page)
   end
 
   def show
