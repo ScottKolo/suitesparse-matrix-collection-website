229ad9632619a2b2f9d2c1502e12ababf43ab69f
Matthew Henderson <matthew.henderson@tamu.edu>
2015-10-29 15:29:50 -0500

    Initial steps toward column hiding

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.2.3'
 gem 'tzinfo-data'
 gem 'tzinfo'
 
+gem 'coffee-script-source', '1.8.0'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 # Use postgresql as the database for Active Record
---
+++ app/assets/javascripts/hiding.js
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////////////
+/// Generate column selectors from a list 
+////////////////////////////////////////////////////////////////////////////////
+var columns = {};
+function generateHides(list) {
+  // Get the selector element
+  var selector = document.getElementById("hide-selector");
+
+  // Generate a selector for each column
+  for(var i = 0; i < list.length; ++i) {
+    var attribute = list[i][0];
+	columns[attribute] = true;
+
+    // Make selector
+    var s = document.createElement("input");
+    s.id = "hide-selector-" + attribute;
+    s.type = "checkbox";
+    s.value = attribute;
+	s.clicked = true;
+    s.onclick = (function(target) {
+      return function() {toggleHide(target);};
+    }(attribute)); // This generates the proper onclick function for each filter.
+
+    // Make selector label
+    var sLabel = document.createElement("label");
+    sLabel.htmlFor = s.id;
+    sLabel.appendChild(document.createTextNode(attribute));
+
+    // Add selector and label to filter-selector
+    selector.appendChild(s);
+    selector.appendChild(sLabel);
+  }
+
+}
+
+function toggleHide(attribute){
+    if(columns[attribute] === true) {
+		columns[attribute] = false;
+	}
+	else {
+		columns[attribute] = true;
+	}
+}
+
+function hideHides() {
+	for(var attribute in columns)
+		toggleHide(attribute);
+}
