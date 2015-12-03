cc3aa64dccf0733dfe435227e1c2932ca484e71f
readamus <readamus@cse.tamu.edu>
2015-12-03 14:31:59 -0600

    Fixed buttons in the slideshows.

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,26 +1,23 @@
-$(window).load(function(){
-		var pages = $('#slide-container li'), current=0;
-		var currentPage,nextPage;
+function setUpSlideShow() {
+  var pages = $('#slide-container li'), current = 0;
+  var currentPage, nextPage;
 
-		$('#slide-container .button').click(function(){
-			currentPage= pages.eq(current);
-			if($(this).hasClass('prevButton'))
-			{
-
-				if (current <= 0)
-					current=pages.length-1;
-				else
-					current=current-1;
-			}
-			else
-			{
-				if (current >= pages.length-1)
-					current=0;
-				else
-					current=current+1;
-			}
-			nextPage = pages.eq(current);	
-			currentPage.hide();	
-			nextPage.show();		
-		});
-});
+  $('#slide-container .button').click(function() {
+    currentPage = pages.eq(current);
+    if($(this).hasClass('prevButton')) {
+      if (current <= 0)
+        current = pages.length - 1;
+      else
+        current = current - 1;
+    }
+    else {
+      if (current >= pages.length - 1)
+        current = 0;
+      else
+        current = current + 1;
+    }
+    nextPage = pages.eq(current);
+    currentPage.hide();
+    nextPage.show();
+  });
+}
