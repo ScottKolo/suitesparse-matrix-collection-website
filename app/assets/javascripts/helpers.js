4ed7f10bc2e2580b3cafb4e1c3b152d9fff3ec55
readamus <readamus@cse.tamu.edu>
2015-12-03 23:40:11 -0600

    Added warnings for javascript/cookies disabled.

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -5,3 +5,14 @@ function setProperties(object, hash) {
   for(var prop in hash)
     object.setAttribute(prop, hash[prop]);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+/// Test cookie storage on the client side.
+////////////////////////////////////////////////////////////////////////////////
+function checkCookies() {
+  var warnings = document.getElementById("warnings");
+  document.cookie = "test=true";
+  if(document.cookie == "") {
+    warnings.innerHTML = "<p>WARNING: this site requires cookies to function! You will not be able to sort or filter without them.</p>";
+  }
+}
