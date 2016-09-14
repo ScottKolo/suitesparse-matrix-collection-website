fc363893398c57f9a05b0bb19b44269e75e3c585
Scott Kolodziej <spkolodziej@gmail.com>
2016-09-14 16:05:13 -0500

    Fix deprecation warning

--- spec/controllers/matrices_controller_spec.rb
+++ spec/controllers/matrices_controller_spec.rb
@@ -18,14 +18,14 @@ RSpec.describe MatricesController, type: :controller do
 
     it "should render the details view when the matrix exists" do
       matrix = create(:matrix, :id => 1)
-      get :show, {:id => matrix.id}
+      get :show, params: {:id => matrix.id}
       expect(response).to render_template :show
     end
 
     it "should show a sad page when the matrix doesn't exist" do
       matrix = double('matrix1')
       allow(matrix).to receive(:id).and_return(1)
-      get :show, {:id => matrix.id}
+      get :show, params: {:id => matrix.id}
       expect(response).to render_template :not_found
     end
 
