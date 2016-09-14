54ddd3b2e85bd0235628c1eca24d9692218f3908
Scott Kolodziej <spkolodziej@gmail.com>
2016-09-13 20:11:20 -0500

    Add rspec tests for matrix sorting scopes

--- spec/models/matrix_spec.rb
+++ spec/models/matrix_spec.rb
@@ -2,11 +2,111 @@ require 'rails_helper'
 require 'factories/matrix'
 
 RSpec.describe Matrix, type: :model do
-  describe "it does nothing" do
-    it "dummy test" do
-      expect(true).to eq(true)
+  describe "sorting scopes" do
+    before(:all) do
+        @matrix1 = FactoryGirl.create(:matrix, 
+          matrix_id: 68,
+          group: "a", 
+          name: "b",
+          num_rows: 1000,
+          num_cols: 1,
+          nonzeros: 1,
+          kind: "2D Mesh",
+          date: 1991
+          )
+        @matrix2 = FactoryGirl.create(:matrix, 
+          matrix_id: 11,
+          group: "B", 
+          name: "c",
+          num_rows: 2000,
+          num_cols: 2,
+          nonzeros: 40,
+          kind: "Fancy type",
+          date: 2001
+          )
+        @matrix3 = FactoryGirl.create(:matrix, 
+          matrix_id: 10000,
+          group: "Z", 
+          name: "D",
+          num_rows: 3000,
+          num_cols: 3,
+          nonzeros: 50,
+          kind: "zeta type",
+          date: 2015
+          )
+    end
+    after(:all) do
+        Matrix.destroy(@matrix1.id)
+        Matrix.destroy(@matrix2.id)
+        Matrix.destroy(@matrix3.id)
+    end
+
+    it "should be able to return a list sorted by id" do
+      @matrices = Matrix.sorted_by("id_asc")
+      expect(@matrices.first).to eq(@matrix2);
+      expect(@matrices.last).to eq(@matrix3);
+      @matrices = Matrix.sorted_by("id_desc")
+      expect(@matrices.first).to eq(@matrix3);
+      expect(@matrices.last).to eq(@matrix2);
+    end
+    it "should be able to return a list sorted by group" do
+      @matrices = Matrix.sorted_by("group_asc")
+      expect(@matrices.first).to eq(@matrix1);
+      expect(@matrices.last).to eq(@matrix3);
+      @matrices = Matrix.sorted_by("group_desc")
+      expect(@matrices.first).to eq(@matrix3);
+      expect(@matrices.last).to eq(@matrix1);
+    end
+    it "should be able to return a list sorted by name" do
+      @matrices = Matrix.sorted_by("name_asc")
+      expect(@matrices.first).to eq(@matrix1);
+      expect(@matrices.last).to eq(@matrix3);
+      @matrices = Matrix.sorted_by("name_desc")
+      expect(@matrices.first).to eq(@matrix3);
+      expect(@matrices.last).to eq(@matrix1);
+    end
+    it "should be able to return a list sorted by number of rows" do
+      @matrices = Matrix.sorted_by("rows_asc")
+      expect(@matrices.first).to eq(@matrix1);
+      expect(@matrices.last).to eq(@matrix3);
+      @matrices = Matrix.sorted_by("rows_desc")
+      expect(@matrices.first).to eq(@matrix3);
+      expect(@matrices.last).to eq(@matrix1);
+    end
+    it "should be able to return a list sorted by number of columns" do
+      @matrices = Matrix.sorted_by("cols_asc")
+      expect(@matrices.first).to eq(@matrix1);
+      expect(@matrices.last).to eq(@matrix3);
+      @matrices = Matrix.sorted_by("cols_desc")
+      expect(@matrices.first).to eq(@matrix3);
+      expect(@matrices.last).to eq(@matrix1);
+    end
+    it "should be able to return a list sorted by number of nonzeros" do
+      @matrices = Matrix.sorted_by("nonzeros_asc")
+      expect(@matrices.first).to eq(@matrix1);
+      expect(@matrices.last).to eq(@matrix3);
+      @matrices = Matrix.sorted_by("nonzeros_desc")
+      expect(@matrices.first).to eq(@matrix3);
+      expect(@matrices.last).to eq(@matrix1);
+    end
+    it "should be able to return a list sorted by kind" do
+      @matrices = Matrix.sorted_by("kind_asc")
+      expect(@matrices.first).to eq(@matrix1);
+      expect(@matrices.last).to eq(@matrix3);
+      @matrices = Matrix.sorted_by("kind_desc")
+      expect(@matrices.first).to eq(@matrix3);
+      expect(@matrices.last).to eq(@matrix1);
+    end
+    it "should be able to return a list sorted by date" do
+      @matrices = Matrix.sorted_by("date_asc")
+      expect(@matrices.first).to eq(@matrix1);
+      expect(@matrices.last).to eq(@matrix3);
+      @matrices = Matrix.sorted_by("date_desc")
+      expect(@matrices.first).to eq(@matrix3);
+      expect(@matrices.last).to eq(@matrix1);
     end
   end
+
 
   describe "getting correct download URL" do
     before(:all) do
