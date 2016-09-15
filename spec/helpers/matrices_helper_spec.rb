dadedf5873f22224573a1fdadec9bb99e6b56001
Scott Kolodziej <spkolodziej@gmail.com>
2016-09-15 09:24:58 -0500

    Add tests for matrices_helper function per_page

---
+++ spec/helpers/matrices_helper_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+require 'factories/matrix.rb'
+require 'support/factory_girl.rb'
+
+RSpec.describe MatricesHelper, type: :helper do
+
+  describe "per_page helper function" do
+    before(:all) do
+        for i in 1..54
+          FactoryGirl.create(:matrix)
+        end
+    end
+    after(:all) do
+        Matrix.delete_all
+    end
+
+    it "should return all the matrices when 'All' is selected" do
+      params = {per_page: "All"}
+      session = {}
+      expect(per_page(params, session)).to eq(54)
+    end
+
+    it "should return 20 matrices when nothing is specified" do
+      params = {}
+      session = {}
+      expect(per_page(params, session)).to eq(20)
+    end
+
+    it "should use session variables when no params are specified" do
+      params = {}
+      session = {per_page: 30}
+      expect(per_page(params, session)).to eq(30)
+    end
+
+    it "should use params variables first" do
+      params = {per_page: 40}
+      session = {per_page: 30}
+      expect(per_page(params, session)).to eq(40)
+    end
+
+  end
+
+end
