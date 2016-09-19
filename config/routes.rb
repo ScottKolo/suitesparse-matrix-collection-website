78c18e90f072d5fccdc2da4d0f9e7987cae176ff
Scott Kolodziej <spkolodziej@gmail.com>
2016-09-18 20:12:44 -0500

    Add matrix submission route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'matrices/new'
 
+  post 'matrices/submit'
+
   resources :matrices
 
   # The priority is based upon order of creation: first created -> highest priority.
