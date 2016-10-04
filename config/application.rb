ccf51775fc0683337a7fef55455a80ac49fd4e07
Scott Kolodziej <spkolodziej@gmail.com>
2016-10-04 16:26:54 -0500

    Enable Deflater compression

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module SuiteSparseMatrixCollectionWebsite
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Enable compression
+    config.middleware.use Rack::Deflater
   end
 end
