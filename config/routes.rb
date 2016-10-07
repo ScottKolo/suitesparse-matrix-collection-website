35c52d5dfb31db06aca3dc492c35acd9a4c891b1
Scott Kolodziej <spkolodziej@gmail.com>
2016-10-07 09:46:05 -0500

    Add routes for submitting matrix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # Root routes to matrix index
   root 'matrices#index'
+  get 'index' => 'matrices#index'
 
   # Static page routes
   # Keep these first to avoid conflict with group_name route
   get 'about' => 'pages#about'
   get 'other_interfaces' => 'pages#other_interfaces'
   get 'submit' => 'matrices#new'
+  post 'submit' => 'matrices#submit'
 
   # Route for sparse.tamu.edu/group_name/matrix_name
   get '/:group/:name', to: 'matrices#show'
