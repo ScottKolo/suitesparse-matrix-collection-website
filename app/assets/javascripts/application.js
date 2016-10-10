0241446e180fefcfa2f931596a9139d77cc40746
Scott Kolodziej <spkolodziej@gmail.com>
2016-10-10 11:12:10 -0500

    Add jquery-turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require filterrific/filterrific-jquery
 //= require js.cookie
-//= require turbolinks
 //= require_tree .
 //= require bootstrap
 //= require jquery.slick
+//= require turbolinks
