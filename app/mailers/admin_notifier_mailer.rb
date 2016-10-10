9f8766ec3237dbfdd0221f92b17ce404d81ea61a
Scott Kolodziej <spkolodziej@gmail.com>
2016-10-09 22:04:22 -0500

    Add to email subject

--- app/mailers/admin_notifier_mailer.rb
+++ app/mailers/admin_notifier_mailer.rb
@@ -6,7 +6,7 @@ class AdminNotifierMailer < ApplicationMailer
   def send_matrix_submitted_email(new_matrix)
     @new_matrix = new_matrix
     mail( to: ENV['DESTINATION_EMAIL'],
-          subject: 'New Matrix Submitted!',
+          subject: '[SuiteSparse Matrix Collection] New Matrix Submitted!',
           template_path: 'admin_notifier',
           template_name: 'new_matrix_email'
      )
