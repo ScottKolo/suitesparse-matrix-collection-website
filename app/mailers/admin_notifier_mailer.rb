cbd14f63c40bd09426469df13f09d2eee4ca6b90
Scott Kolodziej <spkolodziej@gmail.com>
2017-09-14 06:54:09 -0500

    Add reply-to to notification email

--- app/mailers/admin_notifier_mailer.rb
+++ app/mailers/admin_notifier_mailer.rb
@@ -1,11 +1,12 @@
 class AdminNotifierMailer < ApplicationMailer
-  default :from => ENV['EMAIL_ADDRESS']
   add_template_helper(EmailHelpers)
 
   # Send a notification email with the new matrix information
   def send_matrix_submitted_email(new_matrix)
     @new_matrix = new_matrix
     mail( to: ENV['DESTINATION_EMAIL'],
+          from: ENV['EMAIL_ADDRESS'],
+          reply_to: new_matrix.submitter_email,
           subject: '[SuiteSparse Matrix Collection] New Matrix Submitted!',
           template_path: 'admin_notifier',
           template_name: 'new_matrix_email'
