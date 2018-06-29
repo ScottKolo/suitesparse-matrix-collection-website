af402bc4f3d29abd5aa4ab0c8e3f8954f79e01c4
Scott Kolodziej <spkolodziej@gmail.com>
2018-06-29 14:00:10 -0500

    Move recaptcha secrets to secrets.yml

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,7 +1,7 @@
 # config/initializers/recaptcha.rb
 Recaptcha.configure do |config|
-  config.site_key  = ENV['RECAPTCHA_SITE_KEY']
-  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
+  config.site_key  = Rails.application.secrets[:RECAPTCHA_SITE_KEY]
+  config.secret_key = Rails.application.secrets[:RECAPTCHA_SECRET_KEY]
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
