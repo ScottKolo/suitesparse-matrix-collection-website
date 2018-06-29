d07c6eb1b924a3b0b400de159c64a228d55b291c
Scott Kolodziej <spkolodziej@gmail.com>
2018-06-29 11:51:28 -0500

    Fix static file/asset serving configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # Large files are handled by Apache, but we still serve css/js via Rails
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
